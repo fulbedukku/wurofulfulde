cc4cd7c950ddad07f1b6eaaa0990966e65aeac61
fulbedukku <fulbedukku@gmail.com>
2025-10-28 21:14:53 +0000

    base root changes

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -6,7 +6,7 @@ export const SongCard = ({ song }) => {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 m-2">
       <Link to={`/song/${encodedTitle}`}>
-        <img className="rounded-t-lg" src={song.image} alt={song.title} />
+        <img className="rounded-t-lg" src={`${import.meta.env.BASE_URL}`+ song.image} alt={song.title} />
       </Link>
       <div className="p-5">
         <Link to={`/song/${encodedTitle}`}>
@@ -17,7 +17,7 @@ export const SongCard = ({ song }) => {
           to={`/song/${encodedTitle}`}
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Read more
+          Read / Janngu
           <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
           </svg>
