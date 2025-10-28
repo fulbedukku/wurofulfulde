e120168fed0e102c5bbe2455dd9243d2538e8ab2
fulbedukku <fulbedukku@gmail.com>
2025-10-28 13:57:08 +0000

    base root changes

--- src/pages/SongList.jsx
+++ src/pages/SongList.jsx
@@ -5,7 +5,7 @@ export const SongList = () => {
   const [songs, setSongs] = useState([]);
 
   useEffect(() => {
-    fetch(`${import.meta.env.BASE_URL}/gimi_bappa_nasiru.json`)
+    fetch(`${import.meta.env.BASE_URL}assets/gimi_bappa_nasiru.json`)
       .then(response => response.json())
       .then(data => setSongs(data))
       .catch(error => console.error('Error fetching songs:', error));
